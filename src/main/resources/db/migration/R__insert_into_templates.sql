dd7befdb35d7ed9df14c4d19228fa85f17446382
luizh3 <luiz7355608@gmail.com>
2024-11-15 21:34:17 -0300

    feat: #CERUTF-71 [BACK] Micro Serviços para envio de certificados via e-mail, ajustar template

--- src/main/resources/db/migration/R__insert_into_templates.sql
+++ src/main/resources/db/migration/R__insert_into_templates.sql
@@ -11,83 +11,29 @@ VALUES (
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Welcome Email</title>
-        <style>
-            body {
-                font-family: Arial, sans-serif;
-                background-color: #f4f7fa;
-                margin: 0;
-                padding: 0;
-                display: flex;
-                justify-content: center;
-                align-items: center;
-                height: 100vh;
-            }
-            .container {
-                width: 100%;
-                max-width: 600px;
-                background-color: #ffffff;
-                padding: 20px;
-                border-radius: 8px;
-                box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-                text-align: center;
-            }
-            .header img {
-                width: 50px;
-                margin-bottom: 20px;
-            }
-            h1 {
-                font-size: 24px;
-                color: #333333;
-                margin: 0;
-            }
-            p {
-                color: #555555;
-                font-size: 16px;
-                line-height: 1.5;
-            }
-            .btn {
-                display: inline-block;
-                background-color: #3498db;
-                color: #ffffff;
-                padding: 12px 24px;
-                border-radius: 5px;
-                text-decoration: none;
-                font-weight: bold;
-                margin-top: 20px;
-            }
-            .btn:hover {
-                background-color: #2980b9;
-            }
-            .footer {
-                font-size: 14px;
-                color: #888888;
-                margin-top: 20px;
-            }
-            .footer a {
-                color: #3498db;
-                text-decoration: none;
-            }
-
-            .name {
-                font-size: larger;
-            }
-        </style>
     </head>
-    <body>
-        <div class="container">
-            <div class="header">
-                <img th:src="${logoUrl}" alt="Welcome Icon">
-            </div>
-            <h1>Bem-vindo!</h1>
-            <p class="name" >olá <span th:text="${receiverName}">Destinatario</span>,</p>
-            <p> Obrigado por participar. Estamos muito entusiasmados por ter você a bordo. </p>
-            <a th:href="${loginUrl}" class="btn">ENTRAR NA SUA CONTA</a>
-            <div class="footer">
-                <p>Tem alguma pergunta?<br>
-                    Confira nosso <a href="#"> Base de conhecimento</a> para uma resposta rápida.<br>
-                <p>© 2024 <span th:text="${aplicationName}"></span>. All rights reserved.</p>
-            </div>
-        </div>
+    <body style="font-family: Arial, sans-serif; background-color: #ececec; margin: 0; padding: 0; display: flex; justify-content: center; align-items: center; height: 100vh; width: 100%;">
+        <table width="100%" border="0" cellspacing="0" cellpadding="0">
+            <tr>
+                <td align="center">
+                    <div style="width: 600px; height: 355px; background-color: #ffffff; padding: 20px; border-radius: 8px; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1); text-align: center;">
+                        <div style="margin-bottom: 10px;">
+                            <img th:src="${logoUrl}" alt="Welcome Icon" style="width: 50px;">
+                        </div>
+                        <h1 style="font-size: 24px; color: #333333; margin: 0;">Bem-vindo!</h1>
+                        <p class="name" style="font-size: larger;">Olá <span th:text="${receiverName}">Destinatario</span>,</p>
+                        <p style="color: #555555; font-size: 16px; line-height: 1.5;">Seu cadastro foi realizado com sucesso. <br> Estamos muito entusiasmados por ter você a bordo.</p>
+                        <a th:href="${loginUrl}" style="display: inline-block; background-color: #3498db; color: #ffffff; padding: 12px 24px; border-radius: 5px; text-decoration: none; font-weight: bold; margin-top: 5px;">ENTRAR NA SUA CONTA</a>
+                        <div style="font-size: 14px; color: #888888; margin-top: 10px;">
+                            <p>Tem alguma pergunta?<br>
+                                Confira nosso <a href="#" style="color: #3498db; text-decoration: none;">Base de conhecimento</a> para uma resposta rápida.<br>
+                            </p>
+                            <p>© 2024 <span th:text="${aplicationName}"></span>. All rights reserved.</p>
+                        </div>
+                    </div>
+                </td>
+            </tr>
+        </table>
     </body>
     </html>
     $$,
